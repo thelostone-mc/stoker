dbeb3bf7d5361edcda8fac9fb284cd24f6256d8d
Aditya Anand <aditya.anandmc@gmail.com>
2017-06-12 01:00:17 +0530

    database: setup oplog monitoring + socket.io
    Using mongo-oplog to track changes made to database and emitting
    an event via socket.io when data in mongo gets updated.
    
    Refs: #1
    Fixes: #1

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,14 +1,38 @@
 var express = require('express'),
   router = express.Router(),
   mongoose = require('mongoose'),
-  Stock = require('../helpers/stockCRUD');
+  Stock = require('../helpers/stockCRUD'),
+  MongoOplog = require('mongo-oplog');
 
 module.exports = function (app) {
   app.use('/', router);
 
-  io.on('connection', async function (socket) {
-    let stocks = await Stock.getStocks();
-    socket.emit('stocks', { stocks: stocks });
+  io.on('connection', function (socket) {
+
+    const oplog = MongoOplog('mongodb://127.0.0.1:27017/local',
+                    { ns: 'stoker-development.stocks' })
+
+    oplog.tail().then(() => {
+      console.log('oplog: tailing')
+    }).catch(err => console.error(err))
+
+    oplog.on('update', async (doc) => {
+      console.log("oplog: Update");
+      let stocks = await Stock.getStocks();
+      socket.emit('stocks', { stocks: stocks });
+    });
+
+    oplog.on('insert',async (doc) => {
+      console.log("oplog: Insert");
+      let stocks = await Stock.getStocks();
+      socket.emit('stocks', { stocks: stocks });
+    });
+
+    oplog.on('delete', async (doc) => {
+      console.log("oplog: Delete");
+      let stocks = await Stock.getStocks();
+      socket.emit('stocks', { stocks: stocks });
+    });
   });
 };
 
